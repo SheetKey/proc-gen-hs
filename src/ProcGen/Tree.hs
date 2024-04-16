b052c93aec2a1aa04926875d25e0c6ef5fff780d
Will <wveatchw@gmail.com>
2024-04-15 20:53:09 -0400

    trees

--- src/ProcGen/Tree.hs
+++ src/ProcGen/Tree.hs
@@ -2,20 +2,25 @@
 
 module ProcGen.Tree where
 
+import ProcGen.Shape.Bezier
+
+-- linear
+import Linear
+
 -- vector
 import qualified Data.Vector as V 
 
 data BranchMode = Fan | Whorled | AltOpp
 
-data PShape = Spherical          -- 0
-            | Hemispherical      -- 1
-            | Cylindrical        -- 2
-            | TaperedCylindrical -- 3
-            | Flame              -- 4
-            | InverseConical     -- 5
-            | TendFlame          -- 6
-            | Envelope           -- 7
-            | Conical            -- 8
+data PShape = Spherical
+            | Hemispherical
+            | Cylindrical
+            | TaperedCylindrical
+            | Flame
+            | InverseConical
+            | TendFlame
+            | Envelope
+            | Conical
 
 data Parameters = Parameters
   { pShape          :: PShape    
@@ -46,14 +51,6 @@ data Parameters = Parameters
   , pBendV          :: V.Vector Double
   , pBranchDist     :: V.Vector Double
   , pRadiusMod      :: V.Vector Double
-  , pLeafBlosNum    :: Int   
-  , pLeafShape      :: Int   
-  , pLeafScale      :: Double
-  , pLeafScaleX     :: Double
-  , pLeafBend       :: Double
-  , pBlossomShape   :: Int   
-  , pBlossomScale   :: Double
-  , pBlossomRate    :: Double
   , pTropism        :: V3 Double
   , pPruneRatio     :: Double   
   , pPruneWidth     :: Double   
@@ -68,7 +65,6 @@ validateParameters Parameters {..}
   | pLevels <= 0 = Left $ "'pLevels' must be greater than 0."
   | pMaxLevel <= 0 = Left $ "'pMaxLevel' must be greater than 0."
   | pRatio <= 0 = Left $ "'pRation' must be greater than 0."
-  | V.length pBaseSplits  /= (pMaxLevel + 1) = Left $ "'pBaseSplits' must have length 'pMaxLevel + 1'"
   | V.length pBaseSize    /= (pMaxLevel + 1) = Left $ "'pBaseSize' must have length 'pMaxLevel + 1'"
   | V.length pDownAngle   /= (pMaxLevel + 1) = Left $ "'pDownAngle' must have length 'pMaxLevel + 1'"
   | V.length pDownAngleV  /= (pMaxLevel + 1) = Left $ "'pDownAngleV' must have length 'pMaxLevel + 1'"
@@ -88,7 +84,7 @@ validateParameters Parameters {..}
   | V.length pBendV       /= (pMaxLevel + 1) = Left $ "'pBendV' must have length 'pMaxLevel + 1'"
   | V.length pBranchDist  /= (pMaxLevel + 1) = Left $ "'pBranchDist' must have length 'pMaxLevel + 1'"
   | V.length pRadiusMod   /= (pMaxLevel + 1) = Left $ "'pRadiusMod' must have length 'pMaxLevel + 1'"
-  | V.any (< 0) pBaseSplits = Left $ "'pBaseSplits' must be greater than or equal to 0."
+  | pBaseSplits < 0 = Left $ "'pBaseSplits' must be greater than or equal to 0."
   | V.any (<= 0) pLength = Left $ "'pLength' must be greater than 0."
   | V.any (< 0) pTaper = Left $ "'pTaper' must be greater than or equal to 0."
   | V.any (> 3) pTaper = Left $ "'pTaper' must be less than or equal to 3."
@@ -97,15 +93,44 @@ validateParameters Parameters {..}
   | V.any (<= 0) pCurveRes = Left $ "'pCurveRes' must be greater than 0."
   | V.any (< 0) pBranchDist = Left $ "'pBranchDist' must be greater than or equal to 0."
   | V.any (< 0) pRadiusMod = Left $ "'pRadiusMod' must be greater than or equal to 0."
-  | V.any (< 0) pLeafBlosNum = Left $ "'pLeafBlosNum' must be greater than or equal to 0."
-  | V.any (<= 0) pLeafScaleX = Left $ "'pLeafScaleX' must be greater than 0."
-  | V.any (< 0) pLeafBend = Left $ "'pLeafBend' must be greater than or equal to 0."
-  | V.any (> 1) pLeafBend = Left $ "'pLeafBlend' must be less than or equal to 1."
-  | V.any (<= 0) pBlossomScale = Left $ "'pBlossomScale' must be greater than 0."
-  | V.any (< 0) pBlossomRate = Left $ "'pBlossomRate' must be greater than or equal to 0."
-  | V.any (> 1) pBlossomRate = Left $ "'pBlossomRate' must be less than or equal to 1."
-  | V.any (< 0) pPruneRatio = Left $ "'pPruneRatio' must be greater than or equal to 0."
-  | V.any (> 1) pPruneRatio = Left $ "'pPruneRatio' must be less than or equal to 1."
-  | V.any (<= 0) pPruneWidth = Left $ "'pPruneWidth' must be greater than 0."
-  | V.any (< 0) pPruneWidthPeak = Left $ "'pPruneWidthPeak' must be greater than or equal to 0."
+  | pPruneRatio < 0 = Left $ "'pPruneRatio' must be greater than or equal to 0."
+  | pPruneRatio > 1 = Left $ "'pPruneRatio' must be less than or equal to 1."
+  | pPruneWidth <= 0 = Left $ "'pPruneWidth' must be greater than 0."
+  | pPruneWidthPeak < 0 = Left $ "'pPruneWidthPeak' must be greater than or equal to 0."
   | otherwise = Right $ Parameters {..}
+
+data Stem = Stem
+  { sDepth :: Int
+  , sCurve :: Curve Double
+  , sParent :: Maybe Int
+  , sOffset :: Double
+  , sRadiusLimit :: Double
+  , sLength :: Double
+  , sRadius :: Double
+  , sLengthChildMax :: Double
+  , sIndex :: Int
+  }
+  deriving (Show)
+
+data Tree = Tree
+  { tTreeScale :: Double
+  , tBaseLength :: Double
+  , tSplitNumError :: V.Vector Double
+  , tTrunkLength :: Double
+  , tStems :: V.Vector Stem
+  }
+
+-- | Create a "blank" stem
+stemFromDepth :: Int -> Stem
+stemFromDepth sDepth = Stem
+  { sCurve          = V.empty
+  , sParent         = Nothing
+  , sOffset         = 0
+  , sRadiusLimit    = -1
+  , sLength         = 0
+  , sRadius         = 0
+  , sLengthChildMax = 0
+  , sIndex          = -1
+  , ..
+  }
+
