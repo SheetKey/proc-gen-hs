0fe8ebee24fb22916003dc2acc5b9f94d8eaea7a
Will <wveatchw@gmail.com>
2024-04-23 15:54:39 -0400

    tidy up instance

--- src/ProcGen/Shape/MultiBezier.hs
+++ src/ProcGen/Shape/MultiBezier.hs
@@ -14,11 +14,11 @@ import Linear
 -- vector
 import qualified Data.Vector as V
 
-class (Eq a, Floating a, Epsilon a, Bezier (PointDim p) a) => MultiPoint p a | p -> a where
-  type PointDim p
-  toBezier :: (a -> a -> (a -> a)) -> p -> p -> PointDim p
+class (Eq a, Floating a, Epsilon a, Bezier (BezierType p) a) => MultiPoint p a | p -> a where
+  type BezierType p
+  toBezier :: (a -> a -> (a -> a)) -> p -> p -> BezierType p
 
--- | A 'CPH' is a "curve point with handles" for a cubic bezier.
+-- | A 'CubicMP' is a "curve point with handles" for a cubic bezier.
 -- Useful for generation.
 data CubicMP a = CubicMP
   { bControl :: V3 a
@@ -29,20 +29,19 @@ data CubicMP a = CubicMP
   deriving (Show)
 
 instance (Eq a, Floating a, Epsilon a) => MultiPoint (CubicMP a) a where
-  type PointDim (CubicMP a) = CubicBezier a
+  type BezierType (CubicMP a) = CubicBezier a
   toBezier mkTaper (CubicMP (V3 cx0 cy0 cz0) _ (V3 cx1 cy1 cz1) r1)
     (CubicMP (V3 cx3 cy3 cz3) (V3 cx2 cy2 cz2) _ r2)
     = CubicBezier { taperC = mkTaper r1 r2, ..}
 
 newtype (Eq a, Floating a, Epsilon a, Bezier b a) => RawCurve b a = RawCurve (V.Vector b)
 
-class (Eq a, Floating a, Epsilon a, MultiPoint p a, Bezier (PointDim p) a)
+class (Eq a, Floating a, Epsilon a, MultiPoint p a, Bezier (BezierType p) a)
       => MultiBezier c p a | c -> p, p -> a where
-  toRawCurve :: (a -> a -> (a -> a)) -> c -> RawCurve (PointDim p) a
+  toRawCurve :: (a -> a -> (a -> a)) -> c -> RawCurve (BezierType p) a
 
-newtype (Eq a, Floating a, Epsilon a, MultiPoint p a) => Curve p a = Curve (V.Vector p)
+newtype MultiPoint p a => Curve p a = Curve (V.Vector p)
 
-instance {-# OVERLAPPABLE #-}
-  (Eq a, Floating a, Epsilon a, MultiPoint (p a) a) => MultiBezier (Curve (p a) a) (p a) a where
-  toRawCurve mkTaper (Curve curve) = RawCurve $
-    V.zipWith (toBezier mkTaper) (V.init curve) (V.tail curve)
+instance {-# OVERLAPPABLE #-} MultiPoint (p a) a => MultiBezier (Curve (p a) a) (p a) a where
+  toRawCurve mkTaper (Curve curve) =
+    RawCurve $ V.zipWith (toBezier mkTaper) (V.init curve) (V.tail curve)
